d50853896921be16f2e1dd752160fb3727c5d9b5
ismaelcarvalhorosa <dt65estudio@yahoo.com.br>
2019-08-26 14:34:00 -0300

    Exercicio 106  menu help

--- Aula21.py
+++ Aula21.py
@@ -124,6 +124,31 @@ def nota(*n, situa=False):
             r['situação'] = 'Ruim'
     return r  
 
+# 106
+c = ('\033[m',  # 0 = sem cor
+    '\033[0;30;41m', # 1 = vermelho 
+    '\033[0;30;42m', # 2 =  verde 
+    '\033[0;30;43m', # 3 amarelo 
+    '\033[0;30;44m', # 4 = Azul
+    '\033[0;30;45m', # 5 = Roxo 
+    '\033[0;7;40m' # 6 = Branco 
+    );
+
+def ajuda(com):
+    título(f'Assesando o manual \'{com}\'', 4)
+    print(c[6], end='')
+    print(com)
+    print(c[0], end='')
+    help(com)
+    sleep(1)
+
+def título(msg, cor=0):
+    tam= len(msg)
+    print(c[cor], end='')
+    print('~'* tam)
+    print(msg)
+    print('~'*tam)
+    print(c[0], end='')
 
 # ================================================================================
 #                            Programa principal
@@ -160,5 +185,17 @@ if menu =='104':
 # Media mais uma vez ________________________
 if menu =='105':
     resp = nota(7, 5, 6, 5, 9, 10, 8, situa=True)
-    #print(resp)
-    help(nota)
+    print(resp)
+    #help(nota)
+
+# HELP !!! 
+if menu =='106':
+    comando =''
+    while True: 
+        título('Sistema de ajuda PyHelp', 2 )
+        comando = str(input('\n Função ou Biblioteca > '))
+        if comando.upper() == 'FIM':
+            break
+        else:
+            ajuda(comando)
+    título('Até logo', 1)
