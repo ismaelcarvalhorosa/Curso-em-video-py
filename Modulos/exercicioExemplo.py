5d813e78aa5d2d284476d1df4d827979cc0b0b82
ismaelcarvalhorosa <dt65estudio@yahoo.com.br>
2019-08-27 01:26:11 -0300

    Exenplo de moduto

---
+++ Modulos/exercicioExemplo.py
@@ -0,0 +1,9 @@
+import Uteis
+
+num = int(input("Degite um valor "))
+fat=Uteis.fatorial(num)
+d=Uteis.dobro(num)
+t=Uteis.triplo(num)
+print(f'O fatorial de {num} e {fat}')
+print(f'O tobro {d}')
+print(f'O triplo {t}')
