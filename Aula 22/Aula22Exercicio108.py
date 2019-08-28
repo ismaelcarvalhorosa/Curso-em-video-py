0e15ea56020869f704efdfd10e445e880f5540e5
ismaelcarvalhorosa <dt65estudio@yahoo.com.br>
2019-08-28 00:55:54 -0300

    Exercicio 110
    Função que resume todo o resultado

--- Aula 22/Aula22Exercicio108.py
+++ Aula 22/Aula22Exercicio108.py
@@ -3,7 +3,4 @@
 import moeda
 
 p = float(input('Digite um preço $:')) # p reprecenta preço 
-print(f'Metade do valor: {moeda.metade(p, True)}')
-print(f'O dobro: {moeda.dobro(p, True)}')
-print(f'O aumentando 10%: {moeda.aumento(p, 10, True)}')
-print(f'Reduzir 13%: {moeda.diminue(p, 13, True)} ')  
+moeda.resumo(p)
--- Aula 22/moeda.py
+++ Aula 22/moeda.py
@@ -19,3 +19,13 @@ def diminue(preco = 0, taxa = 0, formato=False):
 
 def moedaf(preco = 0, moeda = 'R$', formato=False):
     return f'{moeda} {preco:>8.2f}'.replace('.', ',')
+
+def resumo(preco=0, taxaA=10, taxaR=5):
+    print('-'*40)
+    print('Resomo de valores'.center(40))
+    print('-'*40)
+    print(f'Preço analisado: \t{moedaf(preco)}')
+    print(f'Dobro do preço: \t{dobro(preco, True)}')
+    print(f'Metade do preço: \t{metade(preco, True)}')
+    print(f'Taxa: {taxaA}%, Aumento:\t{aumento(preco, taxaA, True)}')
+    print(f'Taxa:{taxaR}% Retução:\t{diminue(preco, taxaR, True)} ')
